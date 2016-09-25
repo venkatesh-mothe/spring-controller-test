bb6a7d8ce7c09bb5f9486ddee923c26eaced6402
Venkatesh M <venkatesh.mothe@gmail.com>
2016-09-25 14:09:00 +0530

    asf

--- SpringTest1/src/com/home/api/dao/ExampleDAO.java
+++
@@ -1,9 +0,0 @@
-package com.home.api.dao;
-
-import java.util.List;
-
-import com.home.api.model.Employee;
-
-public interface ExampleDAO {
-	public List<Employee> getEmployeeList();
-}
--- SpringTest1/src/com/home/api/service/ExampleService.java
+++
@@ -1,9 +0,0 @@
-package com.home.api.service;
-
-import java.util.List;
-
-import com.home.api.model.Employee;
-
-public interface ExampleService {
-	public List<Employee> getEmployeeListDetails();
-}
