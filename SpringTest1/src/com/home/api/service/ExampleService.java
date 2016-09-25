8accfd0726d80f1cf7b5bae1bce94308e99bb1f6
Venkatesh M <venkatesh.mothe@gmail.com>
2016-09-25 13:32:12 +0530

    sample project

---
+++ SpringTest1/src/com/home/api/service/ExampleService.java
@@ -0,0 +1,9 @@
+package com.home.api.service;
+
+import java.util.List;
+
+import com.home.api.model.Employee;
+
+public interface ExampleService {
+	public List<Employee> getEmployeeListDetails();
+}
